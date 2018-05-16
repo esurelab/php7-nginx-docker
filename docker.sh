ea300bcd0008c8e7d03b2bcdf0f39d97a545f2f5
Chris Ijoyah <chris@esurelab.com>
2018-05-16 10:56:38 +0100

    Added docker configuration for Risk Engine API.

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export DOCKER_HOST_IP=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
-docker rm -f api
-docker run -d --name api -v ~/Projects/www/api:/var/www/html -p 8080:80 -h rig --add-host mysql:$DOCKER_HOST_IP php7-nginx-docker:latest
+docker rm -f drivesly_api risk_engine
+docker run -d --name drivesly_api -v /Users/cijoyah/code/drivesly-api:/var/www/html -p 8080:80 -h drivesly_api --add-host mysql:$DOCKER_HOST_IP php7-nginx-docker:latest
+docker run -d --name risk_engine -v /Users/cijoyah/code/risk-engine-api:/var/www/html -p 8081:80 -h risk_engine --add-host mysql:$DOCKER_HOST_IP php7-nginx-docker:latest
